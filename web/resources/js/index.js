0b1af54468510e4b0d012cea2101490e38347931
hanwoosung <yiok79@naver.com>
2024-11-06 15:23:42 +0900

    인덱스 js수정

--- web/resources/js/index.js
+++ web/resources/js/index.js
@@ -15,7 +15,17 @@ $(function () {
         typing();
     }
 
+    function rainbowText(element) {
+        let colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff',];
+        let colorIndex = Math.floor(Math.random() * colors.length);
+        setInterval(function () {
+            $(element).css('color', colors[colorIndex]);
+            colorIndex = (colorIndex + 1) % colors.length;
+        }, 500);
+    }
+
     $('.container, .additional-content').find('h2, h3, li, p').each(function () {
         typeEffect(this, 75);
+        rainbowText(this);
     });
 });
